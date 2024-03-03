a93a9399bc4424a984fbeb26ab4a33e3d94feacf
redrouter <139449069+redrouter@users.noreply.github.com>
2024-03-03 23:25:49 +0530

    Update 4a.  in a .CSV file implement Least Square Regression algorithm.py

--- 4a.  in a .CSV file implement Least Square Regression algorithm.py
+++ 4a.  in a .CSV file implement Least Square Regression algorithm.py
@@ -1,26 +1,24 @@
-# Importing Libraries
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Set the plot size
+
 plt.rcParams['figure.figsize'] = (12.0, 9.0)
 
-# Preprocessing Input Data   
+  
 data = pd.read_csv('C:/Users/kamle/Downloads/ML/prac4/data.csv')
 
-# Extracting the features X and Y
 X = data.iloc[:, 0].values
 Y = data.iloc[:, 1].values
 
-# Plotting the data points
+
 plt.scatter(X, Y)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Scatter Plot of Data')
 plt.show()
 
-# Building the model
 X_mean = np.mean(X)
 Y_mean = np.mean(Y)
 
@@ -37,10 +35,10 @@ c = Y_mean - m * X_mean
 print("Slope (m):", m)
 print("Intercept (c):", c)
 
-# Making Predictions
+
 Y_pred = m * X + c
 
-# Plotting the regression line
+
 plt.scatter(X, Y, label='Actual Data')
 plt.plot(X, Y_pred, color='red', label='Regression Line')
 plt.xlabel('X')
